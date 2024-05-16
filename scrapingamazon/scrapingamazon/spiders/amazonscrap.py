b583a7ee982fb78c9c52d24f0fc7824c0587b16e
emyrkrkmz <emiryusufkorkmaz2011@gmail.com>
2024-05-16 10:05:49 +0300

    speed_up_25sec

--- scrapingamazon/scrapingamazon/spiders/amazonscrap.py
+++ scrapingamazon/scrapingamazon/spiders/amazonscrap.py
@@ -1,5 +1,6 @@
 import scrapy
 from scrapingamazon.items import Item, asins
+from multiprocessing import Pool
 
 
 ## ASIN => https://www.amazon.com.tr/dp/ASIN_CODE
@@ -13,16 +14,28 @@ class AmazonscrapSpider(scrapy.Spider):
     def start_requests(self):
         for asin in asins:
             url = f'https://www.amazon.com.tr/dp/{asin}'
-            yield scrapy.Request(url=url, callback=self.parse_asin)
+            yield scrapy.Request(url=url, callback=self.parse_asin, meta={'asin': asin})
         
         
     def parse_asin(self, response):
-        title = response.xpath('//span[@id="productTitle"]/text()').get()
+        #title = response.xpath('//span[@id="productTitle"]/text()').get()
+        price = response.xpath('//div[@id="corePriceDisplay_desktop_feature_div"]//span[@class="a-price-whole"]/text()').get()
+        #date = response.xpath('//table[@id="productDetails_detailBullets_sections1"]/tbody/tr[3]/td/text()').get()
         
-        yield {
-            'title': title
-        }
+        # item = Item()
+        # item['asin'] = response.meta['asin']
+        # item['price'] = price
+        # item['date'] = date
         
+        
+        if price is None:
+            # Retry the request
+            yield scrapy.Request(url=response.url, callback=self.parse_asin, meta=response.meta)
+        else:
+            yield {
+                'asin': response.meta['asin'],
+                'price': price,
+            }
         
     
     
